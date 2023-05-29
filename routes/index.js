f61bd7c9d0434f4fef8b13e8acfda4e116b67901
anggitrestuu <123190026@student.upnyk.ac.id>
2023-05-29 21:27:34 +0700

    deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express : This Update Service Users' });
+  res.render('index', { title: 'Express : This 2 Update Service Users' });
 });
 
 module.exports = router;
